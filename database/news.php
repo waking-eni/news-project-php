16443068aa2ea5c80fb225264c5e04fdde8e57cc
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-03-04 11:40:14 +0100

    Update news.php

--- database/news.php
+++ database/news.php
@@ -108,3 +108,24 @@ function getAuthor($id) {
         exit();
     }
 }
+
+Function getNewsByCategory($category) {
+    $conn = $_SESSION['conn'];
+    $sql = "SELECT id FROM news WHERE category = ? ;";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql)) {
+        header("Location: ../public/index.php?error=sqlerror");
+        exit();
+    } else {
+        mysqli_stmt_bind_param($stmt, "s", $category);
+        mysqli_stmt_execute($stmt);
+    }
+    $result = mysqli_stmt_get_result($stmt);
+    if($row = mysqli_fetch_assoc($result)) {
+        return $result;
+    }
+    else {
+        header("Location: ../public/index.php?error=sqlerror");
+        exit();
+    }
+}
