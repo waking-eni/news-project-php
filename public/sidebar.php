9b603161d868daefd08eca307b29cf9ede4df896
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-03-06 11:45:24 +0100

    Update sidebar.php

--- public/sidebar.php
+++ public/sidebar.php
@@ -1,21 +1,27 @@
+<?php
+   include_once __DIR__.'/../database/news.php';
+?>
+
 <div class="col sidebar bg-dark mt-0 h-100">
      <h3 class="white-font">Category</h3>
 
-        <ul class="list-unstyled">
-            <li class="nav-item">
-                <a class="nav-link active green-link" href="#">Category</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active green-link" href="#">Category</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active green-link" href="#">Category</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active green-link" href="#">Category</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active green-link" href="#">Category</a>
-            </li>
-        </ul>
+    <ul class="list-unstyled">
+
+    <?php
+     $categories = getAllCategories();
+
+     if($categories && !empty($categories)) {
+         foreach($categories as $key => $category) {
+
+            echo '<li class="nav-item">';
+                echo '<a class="nav-link active green-link" href="#">'.$category['category'].'</a>';
+            echo '</li>';
+
+         }
+     }
+
+     ?>
+
+    </ul>
+    
 </div>
