965a655b7af7e04e5a74886aa329053fca5e94e9
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-03-06 15:22:49 +0100

    Update logout.inc.php

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -2,4 +2,4 @@
 // we are destroying the session, so that if they go back, they won't be logged in again
 	session_start();
 	session_destroy();
-	header("location:index.php");
+	header("Location: ../public/index.php");
