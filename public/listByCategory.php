e63dbabe861a095fd2b64756ded2df3b1513be14
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-03-09 21:24:43 +0100

    Update listByCategory.php
    Pagination

--- public/listByCategory.php
+++ public/listByCategory.php
@@ -43,6 +43,25 @@
 
                 <div class="main-card">
                     <?php
+
+                    //pagination
+                    //get the current page number
+                    if (isset($_GET['page_no']) && $_GET['page_no']!="") {
+                        $page_no = $_GET['page_no'];
+                    } else {
+                        $page_no = 1;
+                    }
+                    
+                    //set total records per page, offset values etc
+                    $total_records_per_page = 10;
+                    $offset = ($page_no-1) * $total_records_per_page;
+                    $previous_page = $page_no - 1;
+                    $next_page = $page_no + 1;
+                    $adjacents = "2";
+                    $total_records = getNumberOfCategories();
+                    $total_no_of_pages = ceil($total_records / $total_records_per_page);
+                    $second_last = $total_no_of_pages - 1; //total pages minus 1
+
                         $category = $_GET['category'];
 
                         if(!empty($category)) {
@@ -64,6 +83,41 @@
 
                     ?>
                 </div>
+
+
+                <!--pagination-->
+                <div class="navbar navbar-expand navbar-dark bg-dark 
+                    d-flex justify-content-center">
+
+                    <ul class="pagination navbar-nav">
+                        <?php if($page_no > 1){
+                        echo "<li><a href='?page_no=1'>First Page</a></li>";
+                        } ?>
+                            
+                        <li class="page-item sr-only" <?php if($page_no <= 1){ echo "class='disabled'"; } ?>>
+                            <a class="page-link" aria-label="Previous" <?php if($page_no > 1){
+                            echo "href='?page_no=$previous_page'";
+                            } ?>>Previous</a>
+                        </li>
+
+                        <li class="page-item  white-font">
+                            Page <?php echo $page_no." of ".$total_no_of_pages; ?>
+                        </li>
+                            
+                        <li class="page-item sr-only" <?php if($page_no >= $total_no_of_pages){
+                        echo "class='disabled'";
+                        } ?>>
+                            <a class="page-link" aria-label="Next" <?php if($page_no < $total_no_of_pages) {
+                            echo "href='?page_no=$next_page'";
+                            } ?>>Next</a>
+                        </li>
+                        
+                        <?php if($page_no < $total_no_of_pages){
+                        echo "<li><a href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+                        } ?>
+                    </ul>
+                </div>
+                <!--end of pagination-->
 
             </main>
             <!--end of main-->
